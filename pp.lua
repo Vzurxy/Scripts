1b39ab2ad89a6cbcaaf19b97f22da368d67cdf5a
ceg <37312389+Vzurxy@users.noreply.github.com>
2020-12-23 10:12:42 -0500

    Update pp.lua

--- pp.lua
+++ pp.lua
@@ -1,163 +1,223 @@
-local Player = game:GetService("Players").LocalPlayer
-local Camera = workspace.CurrentCamera
-local Mouse = Player:GetMouse()
-local Character = Player.Character
-
-local Model = Instance.new("Model")
-Model.Parent = workspace
-
-local Torso =  Instance.new("Part")
-Torso.Name = "Torso"
-Torso.CanCollide = false
-Torso.Anchored = true
-Torso.Parent = Model
-
-local Head = Instance.new("Part")
-Head.Name = "Head"
-Head.Anchored = true
-Head.CanCollide = false
-Head.Parent = Model
-
-local Humanoid = Instance.new("Humanoid")
-Humanoid.Parent = Model
-
-Torso.Position = Vector3.new(0,9999,0)
-Head.Position = Vector3.new(0,9991,0)
-
-Player.Character = Model
+local ch = game.Players.LocalPlayer.Character
+local prt=Instance.new("Model", workspace)
+local z1 =  Instance.new("Part", prt)
+z1.Name="Torso"
+z1.CanCollide = false
+z1.Anchored = true
+local z2  =Instance.new("Part", prt)
+z2.Name="Head"
+z2.Anchored = true
+z2.CanCollide = false
+local z3 =Instance.new("Humanoid", prt)
+z1.Position = Vector3.new(0,9999,0)
+z2.Position = Vector3.new(0,9991,0)
+ game.Players.LocalPlayer.Character=prt
 wait(5)
-Player.Character = Character
+game.Players.LocalPlayer.Character=ch
 wait(6)
-
-game:GetService('RunService').RenderStepped:Connect(function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = true
+game:GetService('RunService').RenderStepped:connect(function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = true
 end)
+plr = game.Players.LocalPlayer
+mouse = plr:GetMouse()
 
-Character.HumanoidRootPart:Destroy()
+game.Players.LocalPlayer.Character.HumanoidRootPart:Destroy() 
 
-local Humanoid = Instance.new("Humanoid")
-Humanoid.Parent = Player.Character
-Player.Character.Humanoid.Parent = game:GetService("ReplicatedStorage")
+local Hum = Instance.new("Humanoid")
 
-local Root = Player.Character["Right Arm"]
-Root.Name = "HumanoidRootPart"
+Hum.Parent = game.Players.LocalPlayer.Character
+game.Players.LocalPlayer.Character.Humanoid.Parent = game.Lighting
+--game.Players.LocalPlayer.Character:FindFirstChild("Torso").Name = "Head"
+--game.Players.LocalPlayer.Character:FindFirstChild("Head"):Destroy()
+--game.Players.LocalPlayer.Character:FindFirstChild(name):Destroy()
+--game.Players.LocalPlayer.Character.HumanoidRootPart.Name = "GaiPArt"
+local root =game.Players.LocalPlayer.Character["Right Arm"] -- game.Players.LocalPlayer.Character["Left Leg"]
+root.Name = "HumanoidRootPart"
 
-Humanoid.HipHeight = 5
+Hum.HipHeight = 5
 
-Camera.CameraSubject = Character.HumanoidRootPart
+           
+workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.HumanoidRootPart
 
-for i,v in pairs(Player.Character:GetChildren()) do
-    if v.Name ~= Root.Name and v.Name ~="Humanoid" and v.Name ~= "Left Arm" and v.Name ~= "Left Leg" and v.Name ~= "Right Leg" then
-        v:Destroy()
-    elseif v.Name == "Left Arm" or v.Name == "Left Leg" or v.Name == "Right Leg" then
-        local bp = Instance.new("BodyPosition")
-        bp.MaxForce = Vector3.new(99999, 99999, 99999)
-        bp.Parent = v
 
-        local bg = Instance.new("BodyGyro")
-        bg.MaxTorque = Vector3.new(9999999, 9999999, 999999)
-        bg.Parent = v
 
-        spawn(function()
-            bp.D = 300
-            while true do
-                bp.Position = Root.Position + Vector3.new(0, 5.1, 0)
-                wait()
-            end
-        end)
-    elseif v.Name == Root.Name then
-        local bg = Instance.new("BodyGyro")
-        bg.MaxTorque = Vector3.new(9999999, 9999999, 999999)
-        bg.CFrame = CFrame.Angles(0, 0, math.rad(90))
-        bg.Parent = v
-    end
+
+for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+	
+	if v.Name ~=root.Name and v.Name ~="Humanoid" and v.Name ~= "Left Arm" and v.Name ~= "Left Leg" and v.Name ~= "Right Leg" then
+		
+		v:Destroy()
+		
+	elseif v.Name == "Left Arm" then
+		
+		local bp = Instance.new("BodyPosition",v)
+		bp.MaxForce = Vector3.new(99999,99999,99999)
+		local bg = Instance.new("BodyGyro",v)
+		bg.MaxTorque = Vector3.new(9999999,9999999,999999)
+		 
+		spawn(function()
+			bp.D = 300
+			while true do
+				bp.Position = root.Position+Vector3.new(0,1.8+1.8+1.8-0.3,0)
+				wait()
+			end
+			
+		end)
+		
+			elseif v.Name == "Left Leg" then
+		
+		local bp = Instance.new("BodyPosition",v)
+		bp.MaxForce = Vector3.new(99999,99999,99999)
+				local bg = Instance.new("BodyGyro",v)
+		bg.MaxTorque = Vector3.new(9999999,9999999,999999)
+       
+		spawn(function()
+			bp.D = 300
+			while true do
+				bp.Position = root.Position+Vector3.new(0,1.8-0.3,0)
+				wait()
+			end
+			
+		end)
+		
+			elseif v.Name == "Right Leg" then
+		
+		local bp = Instance.new("BodyPosition",v)
+		bp.MaxForce = Vector3.new(99999,99999,99999)
+				local bg = Instance.new("BodyGyro",v)
+		bg.MaxTorque = Vector3.new(9999999,9999999,999999)
+		spawn(function()
+		 bp.D = 300	
+			while true do
+				bp.Position = root.Position+Vector3.new(0,1.8+1.8-0.3,0)
+				wait()
+			end
+			
+		end)
+	elseif v.Name == root.Name then
+		
+					local bg = Instance.new("BodyGyro",v)
+		bg.MaxTorque = Vector3.new(9999999,9999999,999999)
+		bg.CFrame = CFrame.Angles(math.rad(0),math.rad(0),math.rad(90))
+		
+	end
+	
+	
 end
 
-local Flying = true
-local Debounce = true
-local Control = {f = 0, b = 0, l = 0, r = 0}
-local LastControl = {f = 0, b = 0, l = 0, r = 0}
-local MaxSpeed = 120
-local Speed = 15
+--local se = Instance.new("SelectionBox",root)
+--se.Adornee = root
 
-local function Fly()
-    local bg = Instance.new("BodyGyro")
-    bg.P = 9e4
-    bg.MaxTorque = Vector3.new(0, 0, 0)
-    bg.CFrame = Root.CFrame
-    bg.Parent = Root
 
-    local bv = Instance.new("BodyVelocity")
-    bv.Velocity = Vector3.new(0,0,0)
-    bv.MaxForce = Vector3.new(9e9, 9e9, 9e9)
+power = 999999 -- change this to make it more or less powerful
 
-    repeat
-        wait()
-        if Control.l + Control.r ~= 0 or Control.f + Control.b ~= 0 then
-            Speed = Speed + 0.2
-            if Speed > MaxSpeed then
-                Speed = MaxSpeed
-            end
-        elseif not (Control.l + Control.r ~= 0 or Control.f + Control.b ~= 0) and Speed ~= 0 then
-            Speed = Speed-1
-            if Speed < 0 then
-                Speed = 0
-            end
-        end
+power = power*10
 
-        if (Control.l + Control.r) ~= 0 or (Control.f + Control.b) ~= 0 then
-            bv.Velocity = ((Camera.CFrame.LookVector * (Control.f + Control.b)) + ((Camera.CFrame * CFrame.new(Control.l + Control.r, (Control.f + Control.b) *0.2,0).Position) - Camera.CFrame.Position)) * Speed
-            LastControl = {f = Control.f, b = Control.b, l = Control.l, r = Control.r}
-        elseif (Control.l + Control.r) == 0 and (Control.f + Control.b) == 0 and speed ~= 0 then
-            bv.Velocity = ((Camera.CoordinateFrame.lookVector * (lastControl.f+lastControl.b)) + ((Camera.CoordinateFrame * CFrame.new(lastControl.l+lastControl.r,(lastControl.f+lastControl.b)*.2,0).p) - Camera.CFrame.Position)) * Speed
-        else
-            bv.Velocity = Vector3.new(0,0,0)
-        end
-    until not Flying
+---
+wait(.1)
+--local bambam = Instance.new("BodyThrust")
+--bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+--bambam.Force = Vector3.new(power,0,power)
+--bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position 
 
-    Control = {f = 0, b = 0, l = 0, r = 0}
-    LastControl = {f = 0, b = 0, l = 0, r = 0}
-    Speed = 0
-    bg:Destroy()
-    bv:Destroy()
+
+
+
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+
+local plr = game.Players.LocalPlayer
+local torso = root
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 120
+local speed = 15
+
+
+---local bambam = Instance.new("BodyThrust")
+---bambam.Parent = torso
+--bambam.Force = Vector3.new(9999999,0,9999999)
+--bambam.Location = torso.Position
+
+
+---
+
+
+function Fly()
+local bg = Instance.new("BodyGyro", torso)
+bg.P = 9e4
+bg.maxTorque = Vector3.new(0, 0, 0)
+bg.cframe = torso.CFrame
+local bv = Instance.new("BodyVelocity", torso)
+bv.velocity = Vector3.new(0,0,0)
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+repeat wait()
+
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+speed = speed+.2
+if speed > maxspeed then
+speed = maxspeed
+end
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+speed = speed-1
+if speed < 0 then
+speed = 0
+end
+end
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+else
+bv.velocity = Vector3.new(0,0,0)
 end
 
-Mouse.KeyDown:connect(function(Key)
-    Key = Key:lower()
+until not flying
+ctrl = {f = 0, b = 0, l = 0, r = 0}
+lastctrl = {f = 0, b = 0, l = 0, r = 0}
+speed = 0
+bg:Destroy()
+bv:Destroy()
 
-    if Key == "e" then
-        Flying = not Flying
-        if not Flying then
-            Fly()
-        end
-    elseif Key == "w" then
-        Control.f = 1
-    elseif Key == "s" then
-        Control.b = -1
-    elseif Key == "a" then
-        Control.l = -1
-    elseif Key == "d" then
-        Control.r = 1
-    end
+end
+mouse.KeyDown:connect(function(key)
+if key:lower() == "e" then
+if flying then flying = false
+else
+flying = true
+Fly()
+end
+elseif key:lower() == "w" then
+ctrl.f = 1
+elseif key:lower() == "s" then
+ctrl.b = -1
+elseif key:lower() == "a" then
+ctrl.l = -1
+elseif key:lower() == "d" then
+ctrl.r = 1
+end
 end)
+mouse.KeyUp:connect(function(key)
+if key:lower() == "w" then
+ctrl.f = 0
+elseif key:lower() == "s" then
+ctrl.b = 0
+elseif key:lower() == "a" then
+ctrl.l = 0
+elseif key:lower() == "d" then
+ctrl.r = 0
+elseif key:lower() == "r" then
 
-mouse.KeyUp:connect(function(Key)
-    Key = Key:lower()
-    if Key == "w" then
-        Control.f = 0
-    elseif Key == "s" then
-        Control.b = 0
-    elseif Key == "a" then
-        Control.l = 0
-    elseif Key == "d" then
-        Control.r = 0
-    end
+end
 end)
-
 Fly()
 
-for i,v in pairs(Character:GetChildren()) do
-    if v.Name == "CharacterMesh" then
-        v:Destroy()
-    end
-end
+wait()
+game.Players.LocalPlayer.Character.CharacterMesh:Remove()
+game.Players.LocalPlayer.Character.CharacterMesh:Remove()
+game.Players.LocalPlayer.Character.CharacterMesh:Remove()
+game.Players.LocalPlayer.Character.CharacterMesh:Remove()
+game.Players.LocalPlayer.Character.CharacterMesh:Remove()
